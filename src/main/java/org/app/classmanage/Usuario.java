cb46a0204f1b38939193b3e25041f1402c75e34d
Rafhael Gomes <rafhaelgomes.dev@gmail.com>
2025-05-14 19:32:02 -0300

    classe usuário

--- src/main/java/org/app/classmanage/Usuario.java
+++ src/main/java/org/app/classmanage/Usuario.java
@@ -1,9 +1,12 @@
 package org.app.classmanage;
 
+import org.app.classmanage.enums.EnumTipoUsuario;
+
 public class Usuario {
     private String _nome;
     private String _ra;
     private String _senha;
+    private EnumTipoUsuario _tipoUsuario;
 
     public Usuario(String nome, String senha, String ra) {
         this._nome = nome;
@@ -15,7 +18,11 @@ public class Usuario {
         return this._nome;
     }
 
-    public boolean validarSenha(String senha) {
-        return this._senha.equals(senha);
+    public EnumTipoUsuario getTipoUsuario() {
+        return this._tipoUsuario;
+    }
+
+    public String getRa() {
+        return this._ra;
     }
 }
---
+++ src/main/java/org/app/classmanage/enums/EnumTipoUsuario.java
@@ -0,0 +1,5 @@
+package org.app.classmanage.enums;
+
+public enum EnumTipoUsuario {
+    PROFESSOR, ALUNO
+}
