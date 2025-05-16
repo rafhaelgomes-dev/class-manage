23a67ea5e1d1a6a75d685dbd149118ebbd27d186
haniel <hanielsilvasantos3@gmail.com>
2025-05-16 16:16:33 -0300

    criando de notas e faltas, e coloquei metodo no professo para que ele possa acessar o a nota e falta do alunoo.

--- app/src/main/java/com/ads/projetoa3/app/Aluno.java
+++ app/src/main/java/com/ads/projetoa3/app/Aluno.java
@@ -1,9 +1,13 @@
 package com.ads.projetoa3.app;
 
+import java.util.ArrayList;
+
 public class Aluno {
     private String nome;
     private String matricula;
     private String curso;
+    private ArrayList<Falta> faltas = new ArrayList<>();
+    private ArrayList<Nota> notas = new ArrayLIst<>();
 
     public Aluno(String nome, String matricula, String curso) {
         this.nome = nome;
@@ -35,9 +39,49 @@ public class Aluno {
         this.nome = nome;
     }
 
+    //metodo que fizemos para poder adicionar notas e faltas
+    public void adicionarFalta(int qtde, String materia) {
+        faltas.add(new Falta(qtde, materia))
+    }
+
+    public void adicionarNota(double nota, String materia) {
+        notas.add(new Nota(nota, materia));
+    }
+
     public void exibirInfo() {
         System.out.println("Nome: " + nome);
         System.out.println("Matricula: " + matricula);
         System.out.println("Curso: " + curso);
+        System.out.println("\nFaltas:");
+        for (Falta f : faltas) {
+            System.out.println("- " + f.materia + ": " + f.qtde + " faltas");
+        }
+
+        System.out.println("\nNotas:");
+        for (Nota n : notas) {
+            System.out.println("- " + n.materia + ": " + n.notas);
+        }
+
+    // classe interna que fizemos para faltas
+    private static class Falta {
+        int qtde;
+        String materia;
+
+        public Falta(int qtde, String materia) {
+            this.qtde = qtde;
+            this.materia = materia;
+        }
+
+}
+
+    //classe interna que fizemos para notas
+    private static class Nota {
+        double nota;
+        String materia;
+
+        public Nota(double nota, String materia) {
+            this.nota = nota;
+            this.materia = materia;
+        }
     }
 }
--- app/src/main/java/com/ads/projetoa3/app/Professor.java
+++ app/src/main/java/com/ads/projetoa3/app/Professor.java
@@ -6,4 +6,12 @@ public class Professor extends Usuario {
    public Professor(String nome, String email) {
        super(nome, email, EnumTipoUsuario.PROFESSOR);
    }
+
+   public void darFalta(Aluno aluno, int qtde, String materia){
+       aluno.adicionarFalta(qtde, materia);
+   }
+
+   public void darNota(Aluno aluno, double nota, String material) {
+       aluno.adicionarNota.(nota, material);
+   }
 }
